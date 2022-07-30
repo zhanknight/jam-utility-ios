034fe63267ba8d414e50d10564025a3a72522a39
zhanknight <zhanknight@gmail.com>
2022-07-29 20:56:51 -0400

    Initialize project

---
+++ jam-utility-ios/jam_utility_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  jam_utility_iosApp.swift
+//  jam-utility-ios
+//
+//  Created by Zhan Knight on 7/29/22.
+//
+
+import SwiftUI
+
+@main
+struct jam_utility_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
