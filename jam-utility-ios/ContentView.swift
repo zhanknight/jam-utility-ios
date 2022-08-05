bc542853ea1f588e37e4c0eb9dce0d94fa6df426
zhanknight <zhanknight@gmail.com>
2022-08-05 07:57:52 -0400

    Add func to generate all possible scale and root combinations

--- jam-utility-ios/ContentView.swift
+++ jam-utility-ios/ContentView.swift
@@ -13,6 +13,7 @@ struct Scale: Identifiable {
     let id = UUID()
     var quality: String
     var intervals: Array<Int>
+    var root: String = "A"
     // build chords eventually
     func chords(root: String) -> Array<String> {
         return ["chords","go","here"]
@@ -34,8 +35,8 @@ let allNotes = ["A", "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb"
 let allNotesUnique = Array(Set(allNotes))
 
 // the definitions
-let major = Scale(quality: "Major", intervals: [2,4,5,7,9,11])
-let minor = Scale(quality: "Minor", intervals: [2,3,5,7,8,10])
+let major = Scale(quality: "Major", intervals: [2,4,5,7,9,11], root: "B")
+let minor = Scale(quality: "Minor", intervals: [2,3,5,7,8,10], root: "B")
 let dorian = Scale(quality: "Dorian", intervals: [2,3,5,7,9,10])
 let phrygian = Scale(quality: "Phrygian", intervals: [1,3,5,7,8,10])
 let lydian = Scale(quality: "Lydian", intervals: [2,4,6,7,9,11])
@@ -46,17 +47,30 @@ let locrian = Scale(quality: "Locrian", intervals: [1,3,5,6,8,10])
 let allDefinitions = [major, minor, dorian, phrygian, lydian, mixolydian, locrian]
 
 
+// generate all scales with all roots
+func generateAllScales() -> Array<Scale> {
+    var allScales = Array<Scale>()
+    for note in allNotesUnique {
+        for definition in allDefinitions {
+            allScales.append(Scale(quality: definition.quality, intervals: definition.intervals, root: note))
+        }
+    }
+    return allScales
+}
+
 
 // the UI
 struct ContentView: View {
-    
+    var test = generateAllScales()
     @State private var selectedNotes: Set<String> = []
+    @State var resultCount = 0
     
     var body: some View {
         NavigationView {
             ZStack {
                 Color.init(red: 0.2, green: 0.3, blue: 0.5, opacity: 0.5)
                 VStack {
+                    // buttons here
                     HStack {
                         ForEach(allNotesUnique, id: \.self) { note in
                             Button(note) {
@@ -67,16 +81,14 @@ struct ContentView: View {
                                     selectedNotes.insert(note)
                                 }
                                 print(selectedNotes)
-
+                                print(test)
                             }
 
                         }
                     }
+                    // results here
                     List {
-                        
                         ForEach(allDefinitions) { definition in
-                            
-                            
                             ForEach(allNotesUnique, id: \.self) { note in
                                 if definition.notes(root: note).isSuperset(of: selectedNotes) {
                             HStack {
@@ -85,14 +97,14 @@ struct ContentView: View {
                                     Text(definition.notes(root: note).joined(separator: ", "))
                                     Text(definition.chords(root: note).joined(separator: " "))
                                 }
-                            }
+                                }
                             }
                             }
                         }
                         }
                     Text("reset button at the bottom")
                 }
-            }.navigationTitle("Jam Utility")
+            }.navigationTitle("Jam Utility").navigationBarTitleDisplayMode(.inline)
         }
     }
 }
